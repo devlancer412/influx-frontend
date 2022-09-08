b340e1dac8f081e02fc8f21215fcc7fc8b991f90
Joseph <devanderson0412@gmail.com>
2022-09-07 19:43:39 -0700

    logo margin fix

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 
 const Navbar: React.FC = () => {
   return (
-    <div className='w-full h-[56px] bg-[#404040] flex flex-row pr-[58px] pl-[38px] justify-between items-center'>
+    <div className='w-full h-[56px] bg-[#404040] flex flex-row pr-[58px] pl-[30px] justify-between items-center'>
       <Link href='/'>
-        <Image src='/images/mark.png' width='200' height='56' />
+        <Image src='/images/mark.png' width='180' height='56' />
       </Link>
       <div className='py-[8px] px-7 border border-black bg-[#10E98C] hover:cursor-pointer text-[#082129] font-poppins text-[12px] leading-[22px]'>
         List your influencers
