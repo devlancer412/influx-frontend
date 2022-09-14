d522fac061206ae75ede23cd72a97a9f21abd139
Joseph <devanderson0412@gmail.com>
2022-09-14 16:29:49 -0700

    logo changed

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -10,7 +10,12 @@ const Navbar: React.FC<Props> = ({ toggleSidebar }) => {
     <div className='relative w-full h-[56px] bg-[#404040] flex flex-row justify-center md:justify-between items-center md:pr-[58px] md:pl-[25px]'>
       <Link href='/'>
         <div className='hover:cursor-pointer h-full flex flex-row items-center'>
-          <Image src='/images/mark.png' width='180' height='56' />
+          <Image
+            src='/images/logo.svg'
+            width='180'
+            height='65'
+            objectFit='contain'
+          />
         </div>
       </Link>
       <div className='py-[8px] px-7 border border-black bg-[#10E98C] hover:cursor-pointer text-[#082129] font-poppins text-[12px] leading-[22px] hidden md:block'>
