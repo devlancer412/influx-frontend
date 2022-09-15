102d93d5d75214f53631c0f63328ae777ef532ff
Joseph <devanderson0412@gmail.com>
2022-09-14 21:47:16 -0700

    found by infl changed

--- pages/foundbyinfl/index.tsx
+++ pages/foundbyinfl/index.tsx
@@ -32,11 +32,44 @@ const FoundByInfl: React.FC = () => {
             height={300}
             objectFit='contain'
           />
-          <h1 className='flex-1 text-center text-[64px] font-extrabold text-white'>
-            List Your Brand
-          </h1>
+          <div className='flex flex-col items-center'>
+            <h1 className='flex-1 text-center text-[64px] font-extrabold text-white'>
+              List Your Brand
+            </h1>
+            <p className='font-medium text-[15px] leading-[22px] text-[#CCCCCC]'>
+              It may take up to 24 HR for your brand profile to go live due to
+              manual review
+            </p>
+          </div>
           <div className='absolute bottom-0 right-[30%] w-[184px] text-center py-[12px] bg-[#10E98C] text-black font-medium text-[20px] leading-[30px] hover:cursor-pointer'>
             Request Listing
+          </div>
+        </div>
+        <div className='relative flex flex-row items-center justify-between w-[1000px]'>
+          <Image
+            src='/images/03.png'
+            width={245}
+            height={300}
+            objectFit='contain'
+          />
+          <div className='w-[700px] flex flex-col items-center'>
+            <h1 className='flex-1 text-center text-[64px] font-extrabold text-white'>
+              List Your Brand
+            </h1>
+            <p className='font-medium text-[15px] leading-[22px] text-[#CCCCCC]'>
+              Lörem ipsum ingrid Björk Michael Sjögren curlingförälder. Christer
+              Samuelsson Charlotta Gustafsson. Alexandra Berggren Kerstin Öberg
+              Elsa Ali Jessica Ek och Johannes Sundberg. Metasion Hanna
+              Hermansson,
+            </p>
+          </div>
+          <div className='absolute bottom-0 right-[10%] flex flex-row items-center'>
+            <div className='text-center py-[12px] bg-[#10E98C] text-black font-medium text-[20px] leading-[30px] hover:cursor-pointer mr-[22px] px-5'>
+              Requested Vetted badge
+            </div>
+            <div className='text-center py-[12px] border border-[#10E98C] text-white font-medium text-[20px] leading-[30px] hover:cursor-pointer px-5 min-w-[184px]'>
+              More Info
+            </div>
           </div>
         </div>
       </div>
