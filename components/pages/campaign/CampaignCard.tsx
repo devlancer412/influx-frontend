31e2b96a680f829042987ffcf94cc151a6fd5393
Joseph <devanderson0412@gmail.com>
2022-09-15 17:04:26 -0700

    spelling bugfix

--- components/pages/campaign/CampaignCard.tsx
+++ components/pages/campaign/CampaignCard.tsx
@@ -41,7 +41,7 @@ const CampaignCard: React.FC<Campaign> = ({
             </div>
             <div className='flex flex-col justify-between text-center border-b lg:border-b-0 lg:border-r border-[#FFFFFF14] pt-[48px] lg:pt-0'>
               <h5 className='text-[13px] leading-[20px] text-white'>
-                Negaciated Budget
+                Negotiated Budget
               </h5>
               <h5 className='text-[15px] leading-[22px] text-[#10E98C] py-[14px] lg:py-0'>
                 ${budget}K
