19dc46f4f2d0cfcf39b9b0904eaed6e2ce4f5c86
Joseph <devanderson0412@gmail.com>
2022-09-26 21:41:35 -0700

    bugfix 138

--- components/pages/home/InfluenceCard.tsx
+++ components/pages/home/InfluenceCard.tsx
@@ -186,14 +186,14 @@ const InfluenceCard: React.FC<Props> = ({ influence }) => {
         </div>
       </div>
       <div className='flex flex-col px-[35px] py-[14px] w-full'>
-        <div className='relative pt-[19px] pb-[16px] text-start text-[14px] font-semibold text-white border-b border-[#FFFFFF4D]'>
+        {/* <div className='relative pt-[19px] pb-[16px] text-start text-[14px] font-semibold text-white border-b border-[#FFFFFF4D]'>
           <>Audience Size</>
           <p className='absolute text-[#10E98C] top-1/2 -translate-y-1/2 right-0'>
             {influence?.followers > 1000
               ? `${influence?.followers / 1000}K`
               : influence?.followers}
           </p>
-        </div>
+        </div> */}
         <div className='relative pt-[19px] pb-[16px] text-start text-[14px] font-semibold text-white border-b border-[#FFFFFF4D]'>
           <>Engagement</>
           <p className='absolute text-[#10E98C] top-1/2 -translate-y-1/2 right-0'>
