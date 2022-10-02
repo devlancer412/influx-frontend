9847363469848a25bb969597dd0d59f7fe5ec2e8
Joseph <devanderson0412@gmail.com>
2022-10-01 19:47:58 -0700

    chart size

--- components/dialog/influenceprofile/EngagementChart.tsx
+++ components/dialog/influenceprofile/EngagementChart.tsx
@@ -28,11 +28,9 @@ const EngagementChart: React.FC<Props> = ({ data }) => {
   return (
     <div className='mb-[15px] bg-[#324951] rounded-[10px] pt-[16px] px-[2px] pb-[21px] flex flex-col items-center'>
       <div className='flex flex-row justify-center items-center w-full nb-[15px]'>
-        <div className='bg-white w-1 h-1 rounded-full' />
-        <h3 className='text-white text-[11px] mx-1'>
-          Engagement per recent post
+        <h3 className='text-white text-[11px] mx-1 capitalize'>
+          Average Engagements per Post
         </h3>
-        <div className='bg-white w-1 h-1 rounded-full' />
       </div>
       {/* <div className='hidden lg:block'>
         <ResponsiveContainer width={685} aspect={2}>
@@ -54,14 +52,17 @@ const EngagementChart: React.FC<Props> = ({ data }) => {
         </ResponsiveContainer>
       </div> */}
       <div className='hidden md:block'>
-        <ResponsiveContainer width={320} aspect={2}>
+        <ResponsiveContainer width={400} aspect={2}>
           <LineChart
             data={data}
             margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
           >
             <CartesianGrid stroke='#FFFFFF80' vertical={false} />
-            <XAxis dataKey='date' stroke='#FFFFFF80'
-              tick={{ fontSize: '12px' }} />
+            <XAxis
+              dataKey='date'
+              stroke='#FFFFFF80'
+              tick={{ fontSize: '12px' }}
+            />
             <YAxis
               dataKey='engagement'
               stroke='#FFFFFF80'
@@ -82,8 +83,12 @@ const EngagementChart: React.FC<Props> = ({ data }) => {
             margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
           >
             <CartesianGrid stroke='#FFFFFF80' vertical={false} />
-            <XAxis dataKey='date' stroke='#FFFFFF80' fontSize={12}
-              tick={{ fontSize: '12px' }} />
+            <XAxis
+              dataKey='date'
+              stroke='#FFFFFF80'
+              fontSize={12}
+              tick={{ fontSize: '12px' }}
+            />
             <YAxis
               dataKey='data'
               stroke='#FFFFFF80'
